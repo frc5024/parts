26ff611cbab5dc1ea0335976092f5cc1ed9f7d19
Evan Pratten <ewpratten@gmail.com>
2020-02-27 21:51:46 -0500

    Add gradle proj for java server

--- api/application/FlaskApp.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask
-
-app = Flask("5024-Parts-API")
--- api/application/controllers/root.py
+++
@@ -1,9 +0,0 @@
-from ..FlaskApp import app
-from flask import jsonify
-
-
-@app.route("/api")
-def api_root():
-    return jsonify({
-        "success": True
-    })
